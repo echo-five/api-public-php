68552211cd2b3d43c9136cbafff51f42da35a52a
Matthieu Roy <m@matthieuroy.be>
2023-03-06 14:37:52 +0100

    Documentation update

--- examples/get-started-simple-request.php
+++ examples/get-started-simple-request.php
@@ -9,13 +9,13 @@ use EchoFiveApiPublic\EchoFiveApiPublic;
 // Initialize.
 $api = new EchoFiveApiPublic('api.matthieuroy.be', 'MY_API_KEY');
 
-// Execute a request.
+// Do a request.
 $api->request('post', '/api/v1/test/simple', [
     'foo' => 'Bar',
     'biz' => 'Buz',
 ]);
 
-// Get the response.
+// Get the request response.
 $requestResponse = $api->getRequestResponse();
 
 // Dump.
